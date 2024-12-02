5fb7db6366f2720246f4f0c061d9d97b8a591611
MiguelMnzs <miguelricardo_@hotmail.com>
2024-12-02 14:56:25 -0400

    Create Syntax.php

---
+++ Ciencia_Atividade_26_11/Syntax.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <h1>My first PHP page</h1>
+
+    <?php
+    echo "Hello World!<br>";
+    echo "<br>";
+    ?>
+
+    <?php
+    ECHO "Hello World<br>";
+    echo "Hello World<br>";
+    EcHo "Hello World<br>";
+    echo "<br>";
+    ?>
+
+    <?php
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $COLOR . "<br>";
+    echo "My boat is " . $coLOR . "<br>";
+    ?>
+
+
+</body>
+</html>
