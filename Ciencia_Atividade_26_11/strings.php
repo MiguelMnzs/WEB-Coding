1bec33f429e15e4b8dff28b179680ec83ac2d4bd
MiguelMnzs <miguelricardo_@hotmail.com>
2024-12-02 15:01:56 -0400

    Create strings.php

---
+++ Ciencia_Atividade_26_11/strings.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strlen("Hello world!");
+?> 
+
+<?php
+echo str_word_count("Hello world!");
+?> 
+
+<?php
+$x = "Hello World!";
+echo strrev($x);
+?> 
+
+<?php
+echo strpos("Hello world!", "world");
+?> 
+
+<?php
+$x = "Hello World!";
+echo str_replace("World", "Dolly", $x);
+?> 
+ 
+</body>
+</html>
